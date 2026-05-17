1349faf1df33d08949280b95fb3de73d75c56775
name <se90ihou@yahoo.co.jp>
2026-05-17 15:49:17 +0900

    anan

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(100 * 5 + 40 * 8)
+def fizz_buzz(n):
+    if n%15==0:
+        str="FIzzBuzz"
+    elif n%3==0:
+        str="Fizz"
+    elif n%5==0:
+        str="Buzz"
+    else:
+        str=n
+    return str
+
+sizensuu=input("自然数を入力してください。")
+print(fizz_buzz(int(sizensuu)))
+
+    
